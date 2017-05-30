fc40d629f78f8155b047ab7d1ae1df551e20a4ec
Luis Miguel Macias Yagama <G566TEPS@fs.net>
2017-05-30 16:38:30 -0500

    ok

--- storage/cms/cache/15/08/default.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms592de1f7cae2a991634331_804538082Class extends \Cms\Classes\LayoutCode
-{
-
-}
--- storage/cms/cache/31/55/footer.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms592de1f80c362134531280_917879696Class extends \Cms\Classes\PartialCode
-{
-
-}
